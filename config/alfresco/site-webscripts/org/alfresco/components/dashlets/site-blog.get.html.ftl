fa71397a4fc47ab35deca5bcc18e87a025eae4fe
Will Abson <will.abson@gmail.com>
2012-07-18 08:17:54 +0000

    Test if user is a site contributor or greater before offering the Create Post action in Site Blog Dashlet
    git-svn-id: https://share-extras.googlecode.com/svn/trunk/Site Blog Dashlet@1183 a3f5c567-fd0f-3a89-9b71-a290c5a5f590

--- config/alfresco/site-webscripts/org/alfresco/components/dashlets/site-blog.get.html.ftl
+++ config/alfresco/site-webscripts/org/alfresco/components/dashlets/site-blog.get.html.ftl
@@ -10,9 +10,11 @@
 
 <div class="dashlet site-blog-posts">
    <div class="title">${msg("header.siteBlog")}</div>
+<#if userIsSiteContributor>
    <div class="toolbar">
       <a id="${args.htmlid}-createPost-link" class="theme-color-1" title="${msg('list.createLink')}" href="${url.context}/page/site/${page.url.templateArgs.site}/blog-postedit">${msg("list.createLink")}</a>
    </div>
+</#if>
    <div class="body scrollableList" id="${args.htmlid}-body" <#if args.height??>style="height: ${args.height}px;"</#if>>
    </div>
 </div>
--- config/alfresco/site-webscripts/org/alfresco/components/dashlets/site-blog.get.js
+++ config/alfresco/site-webscripts/org/alfresco/components/dashlets/site-blog.get.js
@@ -1,5 +1,18 @@
 function main()
 {
+   // Call the repository to see if the user is site manager or not
+   var userIsSiteContributor = false,
+       json = remote.call("/api/sites/" + page.url.templateArgs.site + "/memberships/" + encodeURIComponent(user.name));
+   
+   if (json.status == 200)
+   {
+      var obj = eval('(' + json + ')');
+      if (obj)
+      {
+         userIsSiteContributor = (obj.role == "SiteContributor" || obj.role == "SiteCollaborator" || obj.role == "SiteManager");
+      }
+   }
+   model.userIsSiteContributor = userIsSiteContributor;
 }
 
 main();
